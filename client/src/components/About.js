8fafcc606215919ef1ab3c637f73bebd520b993c
Umair <zoommair@gmail.com>
2019-04-05 20:33:50 -0500

    About and HowStuffWorks additions Favicon update

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import logo from '../img/LogoWbg.png';
 
 
 class About extends Component {
@@ -6,9 +7,12 @@ class About extends Component {
     return (
       <div className="About">
         <h1>About</h1>
+        <img src={logo} alt="DoUHaveLogo" />
         <h3>The Ethics Company -2018-</h3>
-        <label>Director of Operations:</label><p>David Nguyen</p>
         <label>Director of IT:</label><p>Umair Shahbaz</p>
+        <label>Developer & Analytics:</label><p>Regan Lussier</p>
+        <label>Co-Advisor:</label><p>Airion Watkins-Clark</p>
+        <label>Chief Executive Officer:</label><p>Dave Nguyen</p>
 
       </div>
     );
--- client/src/components/HowItWorks.js
+++ client/src/components/HowItWorks.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import logo from '../img/LogoWbg.png';
 
 
 class HowItWorks extends Component {
@@ -6,17 +7,13 @@ class HowItWorks extends Component {
     return (
       <div className="HowItWorks">
         <h1>How It Works</h1>
-        <p>All items posted on a black page is what people are looking for.
-          Respond to them if you have it and are in the area.
-          We discourage cash and prefer users to use the quality app for currency transitions.</p>
-        <p>People want to make money. People have things laying around that they
-        don’t know it’s worth something. Want to make money? Go to the website and
-        look at what people need and start letting them know you have what they want.</p>
-        <p>Need something? Sign up to create an account where you can post what you need.
-        Get multiple people to submit their offerings to your account so you can review easily
-        and pick one! Its that easy. They come to you! Everything is account based and won’t
-        pester your privacy. You simply get email notification if someone has offered you the
-        item your looking for. We fine items within your budget, distance, and convenience. </p>
+        <img src={logo} alt="DoUHaveLogo" />
+        <h2>Save Money (Find It)</h2>
+        <p>Need something? Sign up to create an account where you can post what you need and the budget you're happy with. They come to you!</p>
+        <h2>Make Money (Sell It)</h2>
+        <p>People want to make money. People have things laying around that they don’t know it’s worth something. Want to make money? Go to the website and look at what people need and start letting them know you have what they want. Search “What people need” Tab and contact them if you have what they are looking for near their location. We discourage cash and prefer users to use the quality one way apps. for transitions.</p>
+        <hr />
+        <p>Please have patience as this is our Beta site and is still in development. A new site that will take time to build up multiple classified ad posts where it is beneficial. Please help us post items that you want.</p>
       </div>
     );
   }
