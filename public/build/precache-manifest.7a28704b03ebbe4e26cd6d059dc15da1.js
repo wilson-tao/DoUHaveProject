000521b20f47ae6fa61bb0a4c50d684abd99319e
Umair <zoommair@gmail.com>
2020-01-21 02:55:18 -0600

    latest build

---
+++ public/build/precache-manifest.7a28704b03ebbe4e26cd6d059dc15da1.js
@@ -0,0 +1,146 @@
+self.__precacheManifest = [
+  {
+    "revision": "924725f8bb78efa48d6d",
+    "url": "/static/css/main.279f17a7.chunk.css"
+  },
+  {
+    "revision": "924725f8bb78efa48d6d",
+    "url": "/static/js/main.924725f8.chunk.js"
+  },
+  {
+    "revision": "cbdf79278c1ab3bc42dc",
+    "url": "/static/css/1.131bf9ce.chunk.css"
+  },
+  {
+    "revision": "cbdf79278c1ab3bc42dc",
+    "url": "/static/js/1.cbdf7927.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "088fd1698f74798d99daf5d6713473a9",
+    "url": "/static/media/001-save-file-option.088fd169.svg"
+  },
+  {
+    "revision": "8cbd86763e7c8853c956dad4064ef154",
+    "url": "/static/media/DoUHaveLogoSmall.8cbd8676.png"
+  },
+  {
+    "revision": "c75b5756ade9914dd9ed3ac85990eb1a",
+    "url": "/static/media/001-facebook-logo-button.c75b5756.svg"
+  },
+  {
+    "revision": "516f1701a7fe48d786a15a00fb11ab3a",
+    "url": "/static/media/003-youtube-logotype.516f1701.svg"
+  },
+  {
+    "revision": "ccf2239d7d34491742af3451a7c2b346",
+    "url": "/static/media/004-linkedin-button.ccf2239d.svg"
+  },
+  {
+    "revision": "5fa7ebf2e1b32cd7634e4e395a4192ea",
+    "url": "/static/media/instagram-logo.5fa7ebf2.svg"
+  },
+  {
+    "revision": "496af0f1482597e8febc7e8fd251bccf",
+    "url": "/static/media/002-share-option.496af0f1.svg"
+  },
+  {
+    "revision": "d995be94c290fb4b64851dd8a05eb011",
+    "url": "/static/media/003-contact.d995be94.svg"
+  },
+  {
+    "revision": "7d596c1c63c5b2b6724c01d0a3b99ffb",
+    "url": "/static/media/LogoWbg.7d596c1c.png"
+  },
+  {
+    "revision": "c28239e1b356a4ab8d17a8881290c841",
+    "url": "/static/media/002-twitter.c28239e1.svg"
+  },
+  {
+    "revision": "dca9c1815458d01aaf5dc02a722cc87b",
+    "url": "/static/media/login.dca9c181.svg"
+  },
+  {
+    "revision": "cd4a4c9841a4e25ab4db8523b1e8cc26",
+    "url": "/static/media/line-menu.cd4a4c98.svg"
+  },
+  {
+    "revision": "31658b245c3b9b2d93f92b47ec596990",
+    "url": "/static/media/001-sedan-car-front.31658b24.svg"
+  },
+  {
+    "revision": "1758951533f84b58080981b66f90eb35",
+    "url": "/static/media/013-washing-machine.17589515.svg"
+  },
+  {
+    "revision": "de6928a8170976eefbd1eb77ba7db3a5",
+    "url": "/static/media/006-cell-phone.de6928a8.svg"
+  },
+  {
+    "revision": "f700c53eb81cbd913a647081f1e9895e",
+    "url": "/static/media/014-couch.f700c53e.svg"
+  },
+  {
+    "revision": "211082164a193f210b95a7e4f351c47b",
+    "url": "/static/media/016-treble-clef.21108216.svg"
+  },
+  {
+    "revision": "a519980cec067c53112e71ed161c12ad",
+    "url": "/static/media/007-motorcycle.a519980c.svg"
+  },
+  {
+    "revision": "f71e24d72e88cad6fc0f5f15ccbbe412",
+    "url": "/static/media/017-console.f71e24d7.svg"
+  },
+  {
+    "revision": "14a46a50e8bf429cb2df87804083bee3",
+    "url": "/static/media/022-lipstick.14a46a50.svg"
+  },
+  {
+    "revision": "e5d8333a1c9cc2887e251468fcde3892",
+    "url": "/static/media/025-customer-support.e5d8333a.svg"
+  },
+  {
+    "revision": "1bd6bf502eafe204a6152c577c3b7d1d",
+    "url": "/static/media/019-clothes.1bd6bf50.svg"
+  },
+  {
+    "revision": "cc6b5b45e61821d1328a7bebad1bfd55",
+    "url": "/static/media/020-sideboard.cc6b5b45.svg"
+  },
+  {
+    "revision": "d3fa70be88a0d768a9af1590484ff7aa",
+    "url": "/static/media/024-car-repair.d3fa70be.svg"
+  },
+  {
+    "revision": "60c01601ae69d9df8ddd01a56cc17383",
+    "url": "/static/media/021-grandfather-clock.60c01601.svg"
+  },
+  {
+    "revision": "75117e2351e5e164635fece98dc77f45",
+    "url": "/static/media/023-forklift-truck.75117e23.svg"
+  },
+  {
+    "revision": "569e8718e1d1244cee61ab3dcd7f229d",
+    "url": "/static/media/004-home.569e8718.svg"
+  },
+  {
+    "revision": "ce6be9f7e7b7d98849cd7c5ab73c917f",
+    "url": "/static/media/005-gift.ce6be9f7.svg"
+  },
+  {
+    "revision": "b1aed51f7a6c63efb554902440a47575",
+    "url": "/static/media/RiverhackSwashes.b1aed51f.otf"
+  },
+  {
+    "revision": "a89f53e866ba44f054eca2d63ef24b30",
+    "url": "/static/media/RIVERHACK_CYR.a89f53e8.otf"
+  },
+  {
+    "revision": "d02e1ec281ee2bee986e8bc69d494f6a",
+    "url": "/index.html"
+  }
+];
